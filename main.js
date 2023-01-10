59c47ac2f1392324a6b02b98f5fbf12ba2d321bf
Anastasiia Popova <popovaanastasiia123@gmail.com>
2023-01-10 14:58:20 +0200

    added my homework to the GitHub

---
+++ main.js
@@ -0,0 +1,12 @@
+let admin;
+let name;
+name = "Анастасія";
+admin = name;
+console.log(admin);
+
+let days = 3;
+let daysSecond = days * (24 * 60 * 60);
+console.log(daysSecond);
+
+let age = prompt("Введіть свій вік") ;
+console.log(`Мені ${age} років`);
